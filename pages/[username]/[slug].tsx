6eaee1a23ef3f8a7fe8523ca73fc89316333a9d6
Cuong Phan <codyreactista@gmail.com>
2022-01-09 21:11:27 +0700

    Hearts Section

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,3 +1,5 @@
+import AuthCheck from "@components/AuthCheck";
+import HeartButton from "@components/HeartButton";
 import Metatags from "@components/Metatags";
 import PostContent from "@components/PostContent";
 import { firestore, getUserWithUsername, postToJSON } from "@lib/firebase";
@@ -9,6 +11,7 @@ import {
   getDoc,
   getDocs,
 } from "firebase/firestore";
+import Link from "next/link";
 import { useDocumentData } from "react-firebase-hooks/firestore";
 
 export default function Post(props) {
@@ -29,6 +32,16 @@ export default function Post(props) {
         <p>
           <strong>{post.heartCount || 0} 🤍</strong>
         </p>
+
+        <AuthCheck
+          fallback={
+            <Link href="/enter" passHref>
+              <button>💗 Sign Up</button>
+            </Link>
+          }
+        >
+          <HeartButton postRef={postRef} />
+        </AuthCheck>
       </aside>
     </main>
   );
